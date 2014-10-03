bb16f38e9aca29b527ea1553642d45fecd1abeca
jelb <jelb@>
2014-10-03 15:15:39 +0200

    færdig med opgave 1.1 og 1.2

--- oo2/oo2_1.c
+++ oo2/oo2_1.c
@@ -1,9 +1,10 @@
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 #include "oo2_1.h"
 
-int sum;					/* this data is shared by the thread(s) */
+double sum;					/* this data is shared by the thread(s) */
 void *runner(void *param);	/* threads call this function */
 
 int main(int argc, char *argv[])
@@ -33,20 +34,17 @@ int main(int argc, char *argv[])
 	
 	pthread_t *pthread_ids;
 	pthread_ids = (pthread_t *) malloc(atoi(argv[2]) * sizeof(pthread_t));
+	Sumjob *jobs;
+	jobs = (Sumjob *) malloc(atoi(argv[2]) * sizeof(Sumjob));
 
-	int first = 0;
-	int last = 0;
 	int interval = atoi(argv[1]) / atoi(argv[2]);
 	int i;
 
 	for (i = 0; i < atoi(argv[2]); i++)
 	{
-		first = last;
-		last = last + interval;
-		Sumjob job;
-		job.first = first*i;
-		job.last = last*++i;
-		pthread_create(&pthread_ids[i],&attr,runner,&job);	/* create the thread */
+		jobs[i].first = interval*i;
+		jobs[i].last = ((i == atoi(argv[2])-1) ? atoi(argv[1]) : interval*(i+1));
+		pthread_create(&pthread_ids[i],&attr,runner,&jobs[i]);	/* create the thread */
 	}
 
 	for (i = 0; i < atoi(argv[2]); i++)
@@ -54,19 +52,21 @@ int main(int argc, char *argv[])
 		pthread_join(pthread_ids[i],NULL);						/* wait for the thread to exit */
 	}
 	
-	printf("sum = %d\n",sum);
+	free(pthread_ids);
+	free(jobs);
+	
+	printf("Threads: %i Sum = %f\n",atoi(argv[2]) ,sum);
 }
 
 /* The thread will begin control in this function */
 void *runner(void *param)
 {
 	Sumjob *job = param;
-	int first = job->first;
-	int last  = job->last;
-	int localsum = 0;
+	double localsum = 0;
+	int i;
 
-	for (first; first < last; first++)
-		localsum += first;
+	for (i = job->first+1; i <= job->last; i++)
+		localsum += sqrt(i);
 
 	sum += localsum;
 
