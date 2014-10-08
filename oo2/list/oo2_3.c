452a5ebb9c3e04772a0b2c7141182b1cbb9b9b07
jelb <jelb@>
2014-10-08 14:08:17 +0200

    k

--- oo2/list/oo2_3.c
+++ oo2/list/oo2_3.c
@@ -2,11 +2,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <semaphores.h>
+#include "list.h"
 
 void *producer(void *param);
 void *comsumer(void *param);
 
-int n;
+int b;
 sem_t *mutex, *full, *empty;
 
 int main(int argc, char *argv[]) {
@@ -19,11 +20,28 @@ int main(int argc, char *argv[]) {
     /*ERROR*/
     }
     
-    if (sem_init(empty, 0, n) != 0) {
+    if (sem_init(empty, 0, b) != 0) {
     /*ERROR*/
     }
     
+    int i;
+	
+	pthread_attr_t attr;
+	pthread_attr_init(&attr);
+	
+	pthread_t *cids;
+	cids = (pthread_t *) malloc(c * sizeof(pthread_t));
     
+    for (i = 0; i < c; i++) {
+        pthread_create(&c_ids[i],&attr,consumer,NULL);
+    }
+        
+    pthread_t *pids;
+	pids = (pthread_t *) malloc(p * sizeof(pthread_t));
+	
+	for (i = 0; i < p; i++) {
+        pthread_create(&pids[i],&attr,producer,NULL);
+    }
 }
 
 void *producer(void *param) {
