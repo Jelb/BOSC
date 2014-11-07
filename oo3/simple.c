1475a018cd2427ed20c67ede7d1b9b7ea0f1c416
Tom Christensen <tom.m.christensen@gmail.com>
2014-11-07 20:11:11 +0100

    Flyttet part 2 fra metode- til implementationsafsnittet

--- oo3/simple.c
+++ oo3/simple.c
@@ -14,7 +14,6 @@ static LIST_HEAD(birthday_list);
 
 void create(int day, int month, int year, struct birthday *person);
 
-/* This function is called when the module is loaded. */
 int simple_init(void)
 {
   struct birthday *person;
@@ -31,34 +30,23 @@ int simple_init(void)
     /* ADDING ELEMENT TO LIST */
     list_add_tail(&person->list, &birthday_list);
   }
-  
   return 0;
 }
 
-
-/* This function is called when the module is removed. */
 void simple_exit(void) {
   struct birthday *ptr, *next;
   printk(KERN_INFO "------[Removing Module]-----\n");
   
-  list_for_each_entry_safe(ptr, next, &birthday_list, list) { 
-	/* on each  iteration ptr points */
-    /* to the next birthday struct */
+  list_for_each_entry_safe(ptr, next, &birthday_list, list) {
     printk(KERN_INFO "Removing birthday (%2d:%2d:%4d)\n", ptr->day, ptr->month, ptr->year);
     list_del(&ptr->list);
     kfree(ptr);
   }
 }
 
-
-
-
-
-/* Macros for registering module entry and exit points. */
 module_init( simple_init );
 module_exit( simple_exit );
 
 MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("Simple Module");
 MODULE_AUTHOR("SGG");
-
